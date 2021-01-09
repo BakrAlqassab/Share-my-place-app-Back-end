b863b66cca2d86f5d4d0f73f7f574d8ad866bef3
BakrAlqassab <33801792+BakrAlqassab@users.noreply.github.com>
2021-01-09 06:33:24 +0200

    rebuild location

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ const bodyParser = require("body-parser");
 const locationRoutes = require("./routes/location");
 
 const app = express();
-// const distDir = __dirname + "/dist/";
-// app.use(express.static(distDir));
-
-// app.set('view engine', 'ejs');
-// app.set('views', 'views');
 
 
 app.use(bodyParser.json());
--- routes/location.js
+++ routes/location.js
@@ -53,7 +53,7 @@ router.get('/location/:lid', (req, res, next) => {
       function(err, doc) {
         // if (err) {}
         if (!doc) {
-          return res.status(404).json({ message: 'Not found!' });
+          return res.status(404).json({ message: 'Not found!!' });
         }
         res.json({ address: doc.address, coordinates: doc.coords });
       }
