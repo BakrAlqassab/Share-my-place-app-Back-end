fa2154492ad32f946b2dc63261838fc7185851df
BakrAlqassab <33801792+BakrAlqassab@users.noreply.github.com>
2021-01-09 04:47:25 +0200

    rebuild package.json

--- app.js
+++ app.js
@@ -9,12 +9,7 @@ const app = express();
 
 // app.set('view engine', 'ejs');
 // app.set('views', 'views');
-if (process.env.NODE_ENV === "production"{
-      app.use(express.static("build"));
-      app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-      });
-    }
+
 
 app.use(bodyParser.json());
 
