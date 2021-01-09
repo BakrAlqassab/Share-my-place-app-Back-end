1b39f9416481118fc4d4f10728b70da46e3cbe83
BakrAlqassab <33801792+BakrAlqassab@users.noreply.github.com>
2021-01-09 05:40:44 +0200

    rebuild location

--- routes/location.js
+++ routes/location.js
@@ -1,88 +1,64 @@
-const express = require("express");
-
-const router = express.Router();
-const mongodb = require("mongodb");
+const express = require('express');
+const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-const assert = require("assert");
+const router = express.Router();
 
-// Connection URL
 const url =
-  "mongodb+srv://Bakr:Bakr2020@cluster0.gk8kb.mongodb.net/locations?retryWrites=true&w=majority";
+  'mongodb+srv://Bakr:Bakr2020@cluster0.gk8kb.mongodb.net/locations?retryWrites=true&w=majority';
 
-// Database Name
-const dbName = "locations";
-
-// Create a new MongoClient
 const client = new MongoClient(url);
 
-router.post("/add-location", (req, res, next) => {
+const locationStorage = {
+  locations: []
+};
+
+router.post('/add-location', (req, res, next) => {
   // const id = Math.random();
-  client.connect(function (err, client) {
-    const db = client.db(dbName);
+  client.connect(function(err, client) {
+    const db = client.db('locations');
 
     // Insert a single document
-    db.collection("user-locations").insertOne(
+    db.collection('user-locations').insertOne(
       {
         address: req.body.address,
-        coords: { lat: req.body.lat, lng: req.body.lng },
+        coords: { lat: req.body.lat, lng: req.body.lng }
       },
-      function (err, r) {
+      function(err, r) {
         // if (err) {}
         console.log(r);
-        res.json({ message: "Stored location!", locId: r.insertedId });
+        res.json({ message: 'Stored location!', locId: r.insertedId });
       }
     );
   });
+
+  // locationStorage.locations.push({
+  //   id: id,
+  //   address: req.body.address,
+  //   coords: { lat: req.body.lat, lng: req.body.lng }
+  // });
 });
 
-router.get("/location/:lid", (req, res, next) => {
+router.get('/location/:lid', (req, res, next) => {
   const locationId = req.params.lid;
-  // const id = Math.random();
-  client.connect(function (err, client) {
-    const db = client.db(dbName);
 
-    // let locationId;
-    // try {
-    //   locationId = new mongodb.ObjectId(locationId);
-    // } catch (error) {
-    //   // return to make sure the other code does not execute
-    //   return res.status(500).json({ message: "Invalid id!" });
-    // }
-    // // END OF ADDED CODE
+  client.connect(function(err, client) {
+    const db = client.db('locations');
 
     // Insert a single document
-    db.collection("user-locations").findOne(
+    db.collection('user-locations').findOne(
       {
-        _id: new mongodb.ObjectId(locationId),
+        _id: new mongodb.ObjectId(locationId)
       },
-      function (err, doc) {
+      function(err, doc) {
         // if (err) {}
         if (!doc) {
-          return res.status(404).json({ message: "Not found!" });
+          return res.status(404).json({ message: 'Not found!' });
         }
         res.json({ address: doc.address, coordinates: doc.coords });
       }
     );
   });
-
-  // client.connect(function (err, client) {
-  //   const db = client.db("locations");
-
-  //   // Insert a single document
-  //   db.collection("user-locations").findOne(
-  //     {
-  //       _id: new mongodb.ObjectId(locationId),
-  //     },
-  //     function (err, doc) {
-  //       // if (err) {}
-  //       if (!doc) {
-  //         return res.status(404).json({ message: "Not found!" });
-  //       }
-  //       res.json({ address: doc.address, coordinates: doc.coords });
-  //     }
-  //   );
-  // });
 });
 
 module.exports = router;
